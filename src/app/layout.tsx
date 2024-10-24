997720f5db6226bbc3d79d646d6288c1364fa647
karthik <karthiktalam8@gmail.com>
2024-10-24 17:21:22 +0530

    chore: Add Vercel speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import StoreProvider from "./StoreProvider";
 import Header from "../components/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Inventory Management",
@@ -22,6 +23,7 @@ export default function RootLayout({
           <main className="p-5">{children}</main>
         </StoreProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
